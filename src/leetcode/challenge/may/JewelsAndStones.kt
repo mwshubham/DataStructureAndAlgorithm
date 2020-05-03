21fc62bef8f7d0f83b27779b9e5ef9a6cd6d69d3
Shubham Agarwal <mwshubham@gmail.com>
2020-05-03 16:06:40 +0530

    leet code -> ransom note problem and added optimized solutions.

---
+++ src/leetcode/challenge/may/JewelsAndStones.kt
@@ -0,0 +1,50 @@
+package leetcode.challenge.may
+
+class JewelsAndStones {
+    companion object {
+        fun numJewelsInStones(J: String, S: String): Int {
+            var result = 0
+            val sortedCharArrayJ = CharArray(52)
+            val sortedIntArrayS = IntArray(52)
+            J.toCharArray().forEach {
+                var asciiValue = it.toByte().toInt()
+//                println("$it: $asciiValue")
+                asciiValue -= if (asciiValue in 65..90) {
+                    65
+                } else {
+                    97 - 26
+                }
+                sortedCharArrayJ[asciiValue] = it
+            }
+//            println("sortedCharArrayJ: ${sortedCharArrayJ.joinToString()}")
+            S.toCharArray().forEach {
+                var asciiValue = it.toByte().toInt()
+                asciiValue -= if (asciiValue in 65..90) {
+                    65
+                } else {
+                    97 - 26
+                }
+                sortedIntArrayS[asciiValue] += 1
+            }
+//            println("sortedIntArrayS: ${sortedIntArrayS.joinToString()}")
+            sortedCharArrayJ.forEachIndexed { index, char ->
+                if (char != '\u0000') {
+                    result += sortedIntArrayS[index]
+                }
+            }
+            return result
+        }
+    }
+// sample 128 ms submission
+//    fun numJewelsInStones(J: String, S: String): Int {
+//        var m = hashMapOf<Char, Int>()
+//        S.forEach { m[it] = m[it]?.plus(1) ?: 1 }
+//        var c = 0
+//        J.forEach { c += m[it] ?: 0 }
+//        return c
+//    }
+}
+
+fun main() {
+    println(JewelsAndStones.numJewelsInStones("aA", "aAAbbbb"))
+}
---
+++ src/leetcode/challenge/may/RansomNote.kt
@@ -0,0 +1,44 @@
+package leetcode.challenge.may
+
+class RansomNote {
+    fun canConstruct(ransomNote: String, magazine: String): Boolean {
+//        return magazine.contains(ransomNote, true)
+        if (ransomNote.isEmpty()) return true
+        if (magazine.isEmpty()) return false
+
+        val ransomNoteMap = hashMapOf<Char, Int>()
+        val magazineMap = hashMapOf<Char, Int>()
+        ransomNote.forEach { index -> ransomNoteMap[index] = ransomNoteMap[index]?.plus(1) ?: 1 }
+        magazine.forEach { index -> magazineMap[index] = magazineMap[index]?.plus(1) ?: 1 }
+
+        ransomNoteMap.entries.forEach { it ->
+            val key = it.key
+            val count = magazineMap[key]
+            if (count == null || count < it.value) {
+                return false
+            }
+        }
+        return true
+    }
+
+//    sample 172 ms submission
+//    fun canConstruct(ransomNote: String, magazine: String): Boolean {
+//        var chars = IntArray(26) { 0 }
+//        for (i in magazine.indices) {
+//            chars[magazine[i] - 'a']++
+//        }
+//        for (i in ransomNote.indices) {
+//            if (--chars[ransomNote[i] - 'a'] < 0) {
+//                return false
+//            }
+//        }
+//        return true
+//    }
+}
+
+fun main() {
+    println(RansomNote().canConstruct("a", "b")) // false
+    println(RansomNote().canConstruct("aa", "ab")) // false
+    println(RansomNote().canConstruct("aa", "aab")) // true
+    println(RansomNote().canConstruct("fffbfg", "effjfggbffjdgbjjhhdegh")) // true
+}
