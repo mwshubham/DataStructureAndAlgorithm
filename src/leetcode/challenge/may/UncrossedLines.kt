25adfbefffce961cb7c2ac7551d9f59940395bb8
Shubham Agarwal <mwshubham@gmail.com>
2020-05-26 09:44:46 +0530

    leet code -> UncrossedLines

---
+++ src/leetcode/challenge/may/UncrossedLines.kt
@@ -0,0 +1,29 @@
+package leetcode.challenge.may
+
+/**
+ *
+ * DP Problem.
+ * https://leetcode.com/submissions/detail/344794293/?from=/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3340/
+ * https://leetcode.com/problems/uncrossed-lines/
+ *
+ * https://dev.to/chakrihacker/uncrossed-lines-leetcode-4h2h
+ */
+class UncrossedLines {
+    fun maxUncrossedLines(A: IntArray, B: IntArray): Int {
+        val sizeA = A.size
+        val sizeB = B.size
+
+        val dp = Array(sizeA) { IntArray(sizeA) }
+
+        for (i in 0 until sizeA) {
+            for (j in 0 until sizeB) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                } else {
+                    dp[i][j] = dp[i - 1][j].coerceAtLeast(dp[i][j - 1])
+                }
+            }
+        }
+        return dp[sizeA][sizeB]
+    }
+}
