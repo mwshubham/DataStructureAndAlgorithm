fbbbfe3f6834be35796d761af73650760a8c3a49
Shubham Agarwal <mwshubham@gmail.com>
2020-07-04 13:24:29 +0530

    DefangingAnIp.kt

---
+++ src/leetcode/problems/array/DefangingAnIp.kt
@@ -0,0 +1,21 @@
+package leetcode.problems.array
+
+import java.lang.StringBuilder
+
+class DefangingAnIp {
+    fun defangIPaddr(address: String): String {
+        val sb = StringBuilder()
+        address.forEach {
+            if (it == '.') {
+                sb.append("[.]")
+            } else {
+                sb.append(it)
+            }
+        }
+        return sb.toString()
+    }
+
+    fun defangIPaddr2(address: String): String {
+        return address.replace(".", "[.]")
+    }
+}
