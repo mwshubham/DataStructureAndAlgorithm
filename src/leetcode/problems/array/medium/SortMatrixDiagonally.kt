743e762472fca5199cb1421cb3b27341d84b316c
Shubham Agarwal <mwshubham@gmail.com>
2020-07-12 11:03:42 +0530

    SortMatrixDiagonally.kt

---
+++ src/leetcode/problems/array/medium/SortMatrixDiagonally.kt
@@ -0,0 +1,24 @@
+package leetcode.problems.array.medium
+
+import java.util.*
+import kotlin.collections.HashMap
+
+class SortMatrixDiagonally {
+    fun diagonalSort(mat: Array<IntArray>): Array<IntArray> {
+        val map = HashMap<Int, PriorityQueue<Int>>()
+        mat.forEachIndexed { row, ints ->
+            ints.forEachIndexed { column, value ->
+                map[row - column]?.add(value)
+                    ?: map.put(row - column, PriorityQueue<Int>().apply {
+                        add(value)
+                    })
+            }
+        }
+        mat.forEachIndexed { row, ints ->
+            ints.forEachIndexed { column, _ ->
+                mat[row][column] = map[row - column]!!.poll()
+            }
+        }
+        return mat
+    }
+}
