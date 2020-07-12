00f6bbde4f92c0ea07c6ba7db6e40ba751b645f8
Shubham Agarwal <mwshubham@gmail.com>
2020-07-12 19:06:07 +0530

    MinStepsToAnagram.kt

---
+++ src/leetcode/problems/string/medium/MinStepsToAnagram.kt
@@ -0,0 +1,20 @@
+package leetcode.problems.string.medium
+
+
+class MinStepsToAnagram {
+
+    fun minSteps(s: String, t: String): Int {
+        val count = IntArray(26)
+        for (i in s.indices) {
+            ++count[s[i] - 'a'] // count the occurrences of chars in s.
+            --count[t[i] - 'a'] // compute the difference between s and t.
+        }
+        var res = 0
+        count.forEach {
+            if (it > 0) {
+                res += it
+            }
+        }
+        return res
+    }
+}
