407aa8787cafafda29294265bd001744670a7ab5
Shubham Agarwal <mwshubham@gmail.com>
2020-10-10 21:31:35 +0530

    ShuffleString.kt

---
+++ src/leetcode/problems/ShuffleString.kt
@@ -0,0 +1,17 @@
+package leetcode.problems
+
+import java.lang.StringBuilder
+
+/**
+ * https://leetcode.com/problems/shuffle-string/
+ */
+class ShuffleString {
+    fun restoreString(s: String, indices: IntArray): String {
+        val array = s.toCharArray()
+        val result = CharArray(array.size)
+        for (i in array.indices) {
+            result[indices[i]] = array[i]
+        }
+        return String(result)
+    }
+}
