e89e7c404a5bc12d5881e2ebf0cacca7d65fc870
Shubham Agarwal <mwshubham@gmail.com>
2021-03-21 12:34:58 +0530

    SameTree.kt

---
+++ src/leetcode/problems/tree/easy/SameTree.kt
@@ -0,0 +1,19 @@
+package leetcode.problems.tree.easy
+
+import leetcode.problems.TreeNode
+
+class SameTree {
+    companion object {
+        /**
+         * Runtime: 132 ms, faster than 94.85% of Kotlin online submissions for Same Tree.
+         * Memory Usage: 33.5 MB, less than 54.64% of Kotlin online submissions for Same Tree.
+         */
+        private fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+            if (p == null && q == null) return true
+            if (p == null || q == null) return false
+            return if (p.`val` == q.`val`) {
+                isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+            } else false
+        }
+    }
+}
