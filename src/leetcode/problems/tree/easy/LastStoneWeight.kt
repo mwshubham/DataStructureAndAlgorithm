ffd6f8f9d0d4187e10273ea71116415b24a1f5e2
Shubham Agarwal <mwshubham@gmail.com>
2021-03-21 13:02:19 +0530

    LastStoneWeight.kt

---
+++ src/leetcode/problems/tree/easy/LastStoneWeight.kt
@@ -0,0 +1,37 @@
+package leetcode.problems.tree.easy
+
+import java.util.*
+
+/**
+ * https://leetcode.com/problems/last-stone-weight/
+ */
+class LastStoneWeight {
+    companion object {
+        /**
+         * Runtime: 152 ms, faster than 68.42% of Kotlin online submissions for Last Stone Weight.
+         * Memory Usage: 33.8 MB, less than 65.79% of Kotlin online submissions for Last Stone Weight.
+         */
+        fun lastStoneWeight(stones: IntArray): Int {
+            val heap = PriorityQueue<Int>(reverseOrder())
+            stones.forEach { heap.add(it) }
+            while (heap.isNotEmpty()) {
+                if (heap.size == 1) {
+                    return heap.poll()
+                }
+                val first = heap.poll()
+                val second = heap.poll()
+                (first - second)
+                    .takeIf { it != 0 }
+                    ?.let {
+                        heap.add(it)
+                    }
+
+            }
+            return 0
+        }
+    }
+}
+
+fun main() {
+    LastStoneWeight.lastStoneWeight(intArrayOf(2, 7, 4, 1, 8, 1))
+}
