ed2b1f9ee8eb4014839d5513f9a498b2fda8ef57
Shubham Agarwal <mwshubham@gmail.com>
2021-04-02 12:36:30 +0530

    RemoveKDigitMakeSmallNumber

--- src/leetcode/problems/stack/medium/RemoveKDigitMakeSmallNumber.kt
+++ src/leetcode/problems/stack/medium/RemoveKDigitMakeSmallNumber.kt
@@ -1,8 +1,18 @@
 package leetcode.problems.stack.medium
 
+import java.util.*
+
+/**
+ * https://leetcode.com/problems/remove-k-digits/
+ */
 // https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3328/
 // https://leetcode.com/submissions/detail/339131847/?from=/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3328/
 class RemoveKDigitMakeSmallNumber {
+
+    /**
+     * Runtime: 220 ms, faster than 42.86% of Kotlin online submissions for Remove K Digits.
+     * Memory Usage: 36.6 MB, less than 42.86% of Kotlin online submissions for Remove K Digits.
+     */
     @Suppress("SpellCheckingInspection")
     fun removeKdigits(num: String, k: Int): String {
         if (num.length == k) return "0"
@@ -21,13 +31,41 @@ class RemoveKDigitMakeSmallNumber {
     }
 
     private fun mapOutput(input: String) = if (input.isEmpty()) "0" else input
+
+
+    fun removeKDigitsUsingStack(num: String, k: Int): String {
+        val stack = Stack<Char>()
+        var K = k
+        num.forEach { char ->
+            while (stack.isNotEmpty() && K > 0 && stack.peek() > char) {
+                stack.pop()
+                K -= 1
+            }
+
+            // If stack has some number
+            // Skip first '0' leading zero
+            if (stack.isNotEmpty() || char != '0') {
+                stack.push(char)
+            }
+        }
+
+
+        // Remove largest values from top of the stack
+        while (stack.isNotEmpty() && K > 0) {
+            stack.pop()
+            K -= 1
+        }
+
+        if (stack.isEmpty()) return "0"
+        return stack.joinToString("")
+    }
 }
 
 fun main() {
     val instance = RemoveKDigitMakeSmallNumber()
 
-    println(instance.removeKdigits("1432219", 3)) // 1219
-    println(instance.removeKdigits("10200", 1)) // 200
-    println(instance.removeKdigits("10", 2)) // 0
-    println(instance.removeKdigits("10", 1)) // 0
+    println(instance.removeKDigitsUsingStack("1432219", 3)) // 1219
+    println(instance.removeKDigitsUsingStack("10200", 1)) // 200
+    println(instance.removeKDigitsUsingStack("10", 2)) // 0
+    println(instance.removeKDigitsUsingStack("10", 1)) // 0
 }
