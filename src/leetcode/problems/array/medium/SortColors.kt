53f2e31139446b43ad65ba145f19b132839f0161
Shubham Agarwal <mwshubham@gmail.com>
2021-04-07 12:37:41 +0530

    SortColors.kt

---
+++ src/leetcode/problems/array/medium/SortColors.kt
@@ -0,0 +1,32 @@
+package leetcode.problems.array.medium
+
+/**
+ * https://leetcode.com/problems/sort-colors/
+ */
+class SortColors {
+    fun sortColors(nums: IntArray): Unit {
+        var l = 0
+        var m = 0
+        var h = nums.lastIndex
+        while (m <= h) {
+            when (nums[m]) {
+                0 -> {
+                    val temp = nums[l]
+                    nums[l] = nums[m]
+                    nums[m] = temp
+                    l++
+                    m++
+                }
+                1 -> {
+                    m++
+                }
+                2 -> {
+                    val temp = nums[h]
+                    nums[h] = nums[m]
+                    nums[m] = temp
+                    h--
+                }
+            }
+        }
+    }
+}
