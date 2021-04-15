f2bab6519c4a12f7a84f35f232c0d7df0e2abdca
Shubham Agarwal <mwshubham@gmail.com>
2021-04-15 17:23:41 +0530

    Refactor TRIE implementation code

--- src/leetcode/problems/trie/medium/ImplementTriePrefixTree.kt
+++ src/leetcode/problems/trie/medium/ImplementTriePrefixTree.kt
@@ -2,6 +2,7 @@ package leetcode.problems.trie.medium
 
 /**
  * https://leetcode.com/problems/implement-trie-prefix-tree/
+ * https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3329/
  */
 class TrieNode(
     val char: Char,
