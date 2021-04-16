f6a4f28ba409d49ab944ae7cfe4995b642fc8da8
Shubham Agarwal <mwshubham@gmail.com>
2021-04-16 11:43:13 +0530

    KthSmallestBST.kt

--- src/leetcode/problems/tree/medium/KthSmallestBST.kt
+++ src/leetcode/problems/tree/medium/KthSmallestBST.kt
@@ -1,14 +1,21 @@
-package leetcode.challenge.may
+package leetcode.problems.tree.medium
 
 import datastructure.TreeNode
 
-// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3335/
-// https://leetcode.com/submissions/detail/342459032/?from=/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3335/
+/**
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+ * https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3335/
+ * https://leetcode.com/submissions/detail/342459032/?from=/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3335/
+ */
 class KthSmallestBST {
 
     private lateinit var array: MutableList<Int>
     private var k = 0
 
+    /**
+     * Runtime: 196 ms, faster than 88.37% of Kotlin online submissions for Kth Smallest Element in a BST.
+     * Memory Usage: 36.5 MB, less than 79.07% of Kotlin online submissions for Kth Smallest Element in a BST.
+     */
     fun kthSmallest(root: TreeNode, k: Int): Int {
         array = mutableListOf()
         this.k = k
