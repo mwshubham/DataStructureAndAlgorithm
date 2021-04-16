525c97e920b00b6a4796109592228ec69fb5680f
Shubham Agarwal <mwshubham@gmail.com>
2021-04-16 11:21:53 +0530

    MiddleOfLinkedList.kt

--- src/leetcode/problems/linkedlist/easy/MiddleOfLinkedList.kt
+++ src/leetcode/problems/linkedlist/easy/MiddleOfLinkedList.kt
@@ -2,6 +2,7 @@ package leetcode.problems.linkedlist.easy
 
 import datastructure.ListNode
 
+
 class MiddleOfLinkedList {
     fun middleNode(head: ListNode?): ListNode? {
         var node = head
@@ -11,5 +12,19 @@ class MiddleOfLinkedList {
             node = node.next
         }
         return list[list.size / 2]
+    }
+
+    /**
+     * Runtime: 124 ms, faster than 90.28% of Kotlin online submissions for Middle of the Linked List.
+     * Memory Usage: 33.4 MB, less than 19.44% of Kotlin online submissions for Middle of the Linked List.
+     */
+    fun middleNodeUsingSlowAndFastPointer(head: ListNode?): ListNode? {
+        var slow: ListNode? = head
+        var fast: ListNode? = head
+        while (fast?.next != null) {
+            slow = slow!!.next
+            fast = fast.next!!.next
+        }
+        return slow
     }
 }
