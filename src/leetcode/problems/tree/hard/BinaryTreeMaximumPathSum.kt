d0efb8b95120d00f9c269a59af6099235232ffb4
Shubham Agarwal <mwshubham@gmail.com>
2021-04-18 12:24:26 +0530

    BinaryTreeMaximumPathSum.kt

---
+++ src/leetcode/problems/tree/hard/BinaryTreeMaximumPathSum.kt
@@ -0,0 +1,32 @@
+package leetcode.problems.tree.hard
+
+import leetcode.problems.TreeNode
+import kotlin.math.max
+
+/**
+ * 124. Binary Tree Maximum Path Sum
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/
+ */
+class BinaryTreeMaximumPathSum {
+    companion object {
+
+        var maxValue = Int.MIN_VALUE
+
+        /**
+         * Runtime: 212 ms, faster than 69.23% of Kotlin online submissions for Binary Tree Maximum Path Sum.
+         * Memory Usage: 38.9 MB, less than 55.56% of Kotlin online submissions for Binary Tree Maximum Path Sum.
+         */
+        fun maxPathSum(root: TreeNode?): Int {
+            maxPathDown(root)
+            return maxValue
+        }
+
+        fun maxPathDown(node: TreeNode?): Int {
+            node ?: return 0
+            val left = max(0, maxPathDown(node.left))
+            val right = max(0, maxPathDown(node.right))
+            maxValue = max(maxValue, left + right + node.`val`)
+            return max(left, right) + node.`val`
+        }
+    }
+}
