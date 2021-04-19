667f1184bcfcb73cdd3ce1d6fa4cd0c8e2e6329f
Shubham Agarwal <mwshubham@gmail.com>
2021-04-19 12:34:47 +0530

    LRUCache.kt

--- src/LRUCache.kt
+++ src/LRUCache.kt
@@ -1,12 +1,62 @@
 import java.util.*
 import kotlin.collections.HashSet
 
+
+class Node(var key: Int, var value: Int) {
+    var prev: Node? = null
+    var next: Node? = null
+}
+
 class LRUCache(private val capacity: Int) {
 
     /*[1, 2, 5, 4]*/
 //    private val queue = LinkedList<Int>()
     private val queue = ArrayDeque<Int>()
     private val hashset = HashSet<Int>()
+
+    var head = Node(0, 0).apply {
+        next = tail
+    }
+    var tail: Node? = Node(0, 0).apply {
+        prev = null
+    }
+
+    var map = HashMap<Int, Node>()
+
+    fun get(key: Int): Int {
+        return if (map.containsKey(key)) {
+            val node = map[key]!!
+            remove(node)
+            insert(node)
+            node.value
+        } else {
+            -1
+        }
+    }
+    fun put(key: Int, value: Int) {
+        if (map.containsKey(key)) {
+            remove(map[key]!!)
+        }
+        if (map.size == capacity) {
+            remove(tail!!.prev!!)
+        }
+        insert(Node(key, value))
+    }
+
+    private fun remove(node: Node) {
+        map.remove(node.key)
+        node.prev?.next = node.next
+        node.next?.prev = node.prev
+    }
+
+    private fun insert(node: Node) {
+        map[node.key] = node
+        val headNext = head.next
+        head.next = node
+        node.prev = head
+        headNext?.prev = node
+        node.next = headNext
+    }
 
     fun refer(element: Int) {
         if (hashset.contains(element)) {
