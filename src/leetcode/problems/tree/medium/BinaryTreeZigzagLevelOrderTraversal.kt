b546a4a3f310563e464d41fdd8fd9ace2e79b3a0
Shubham Agarwal <mwshubham@gmail.com>
2021-04-19 12:34:41 +0530

    BinaryTreeZigzagLevelOrderTraversal.kt

---
+++ src/leetcode/problems/tree/medium/BinaryTreeZigzagLevelOrderTraversal.kt
@@ -0,0 +1,57 @@
+package leetcode.problems.tree.medium
+
+import leetcode.problems.TreeNode
+import java.util.*
+import kotlin.collections.ArrayList
+
+/**
+ * 103. Binary Tree Zigzag Level Order Traversal
+ * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+ */
+class BinaryTreeZigzagLevelOrderTraversal {
+    companion object {
+
+        /**
+         * Runtime: 172 ms, faster than 75.70% of Kotlin online submissions for Binary Tree Zigzag Level Order Traversal.
+         * Memory Usage: 35.8 MB, less than 50.47% of Kotlin online submissions for Binary Tree Zigzag Level Order Traversal.
+         */
+        fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
+            val result = ArrayList<List<Int>>()
+            root ?: return result
+
+            result.add(listOf(root.`val`))
+
+            val listOfTreeNodes = LinkedList<TreeNode>()
+            listOfTreeNodes.add(root)
+            var readReverse = true
+            while (listOfTreeNodes.isNotEmpty()) {
+                val listOfValues = ArrayList<Int>()
+                repeat(listOfTreeNodes.size) {
+                    listOfTreeNodes.removeFirst().let { node ->
+                        node.left?.let { child ->
+                            if (readReverse) {
+                                listOfValues.add(0, child.`val`)
+                            } else {
+                                listOfValues.add(child.`val`)
+                            }
+                            listOfTreeNodes.add(child)
+                        }
+                        node.right?.let { child ->
+                            if (readReverse) {
+                                listOfValues.add(0, child.`val`)
+                            } else {
+                                listOfValues.add(child.`val`)
+                            }
+                            listOfTreeNodes.add(child)
+                        }
+                    }
+                }
+                readReverse = !readReverse
+                if (listOfValues.isNotEmpty()) {
+                    result.add(listOfValues)
+                }
+            }
+            return result
+        }
+    }
+}
