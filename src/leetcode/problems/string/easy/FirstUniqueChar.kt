c57815114a4fa99660f8b17c56f3e842beee06b8
Shubham Agarwal <mwshubham@gmail.com>
2021-04-19 19:12:42 +0530

    FirstUniqueChar.kt

--- src/leetcode/problems/string/easy/FirstUniqueChar.kt
+++ src/leetcode/problems/string/easy/FirstUniqueChar.kt
@@ -1,8 +1,17 @@
 package leetcode.problems.string.easy
 
+/**
+ * 387. First Unique Character in a String
+ * https://leetcode.com/problems/first-unique-character-in-a-string/
+ */
 class FirstUniqueChar {
 
     companion object {
+
+        /**
+         * Runtime: 248 ms, faster than 80.00% of Kotlin online submissions for First Unique Character in a String.
+         * Memory Usage: 36.1 MB, less than 84.00% of Kotlin online submissions for First Unique Character in a String.
+         */
         @Suppress("SpellCheckingInspection")
         fun firstUniqChar(input: String): Int {
             val countMap = hashMapOf<Char, Int>()
@@ -16,13 +25,16 @@ class FirstUniqueChar {
         }
 
 
-        // sample 232 ms submission
-//        fun firstUniqChar(input: String): Int {
-//            val map = IntArray(26)
-//            input.forEach { char -> map[char - 'a']++ }
-//            input.forEachIndexed { index, char -> if (map[char - 'a'] == 1) return index }
-//            return -1
-//        }
+        /**
+         * Runtime: 208 ms, faster than 97.14% of Kotlin online submissions for First Unique Character in a String.
+         * Memory Usage: 36.2 MB, less than 50.86% of Kotlin online submissions for First Unique Character in a String.
+         */
+        fun firstUniqCharUsingArray(input: String): Int {
+            val map = IntArray(26)
+            input.forEach { char -> map[char - 'a']++ }
+            input.forEachIndexed { index, char -> if (map[char - 'a'] == 1) return index }
+            return -1
+        }
     }
 }
 
