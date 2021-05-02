5926419d755cb0e87b4ce9bfe203e344fbd62f87
Shubham Agarwal <mwshubham@gmail.com>
2021-05-02 12:52:56 +0530

    1844. Replace All Digits with Characters

--- src/leetcode/contest/ReplaceAllDigitsWithCharacters.kt
+++ src/leetcode/contest/ReplaceAllDigitsWithCharacters.kt
@@ -3,8 +3,16 @@ package leetcode.contest
 import kotlin1.println
 import java.lang.StringBuilder
 
+/**
+ * 1844. Replace All Digits with Characters
+ * https://leetcode.com/problems/replace-all-digits-with-characters/
+ */
 class ReplaceAllDigitsWithCharacters {
     companion object {
+        /**
+         * Runtime: 152 ms, faster than 100.00% of Kotlin online submissions for Replace All Digits with Characters.
+         * Memory Usage: 33.8 MB, less than 100.00% of Kotlin online submissions for Replace All Digits with Characters.
+         */
         fun replaceDigits(s: String): String {
             val sb = StringBuilder()
             s.forEachIndexed { index, c ->
