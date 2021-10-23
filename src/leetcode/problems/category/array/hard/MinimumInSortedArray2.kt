8719947fcd68867b968a62b87a860e8ea064a0d3
Shubham Agarwal <mwshubham@gmail.com>
2021-10-23 14:52:10 +0530

    154. Find Minimum in Rotated Sorted Array II

--- src/leetcode/problems/category/array/hard/MinimumInSortedArray2.kt
+++ src/leetcode/problems/category/array/hard/MinimumInSortedArray2.kt
@@ -3,7 +3,15 @@ package leetcode.problems.category.array.hard
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3401/
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
 // https://leetcode.com/submissions/detail/371730027/?from=/explore/challenge/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3401/
+/**
+ * 154. Find Minimum in Rotated Sorted Array II
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+ */
 class MinimumInSortedArray2 {
+    /**
+     * Runtime: 188 ms, faster than 81.58% of Kotlin online submissions for Find Minimum in Rotated Sorted Array II.
+     * Memory Usage: 38.8 MB, less than 18.42% of Kotlin online submissions for Find Minimum in Rotated Sorted Array II.
+     */
     fun findMin(nums: IntArray): Int {
         var l = 0
         var r = nums.lastIndex
