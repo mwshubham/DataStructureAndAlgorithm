f555883c221d89f3e0b96b71ad26a1d233cd5f5c
Shubham Agarwal <mwshubham@gmail.com>
2021-12-04 19:52:27 +0530

    191. Number of 1 Bits

---
+++ src/leetcode/NumberOf1Bits.kt
@@ -0,0 +1,26 @@
+package leetcode
+
+import kotlin1.println
+
+/**
+ * 191. Number of 1 Bits
+ * https://leetcode.com/problems/number-of-1-bits/
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/565/
+ */
+class NumberOf1Bits {
+    companion object {
+        fun hammingWeight(n: Int): Int {
+            var out = 0
+            var num = n
+            while (num != 0) {
+                out += (num and 1)
+                num = num ushr 1
+            }
+            return out
+        }
+    }
+}
+
+fun main() {
+    NumberOf1Bits.hammingWeight(521).println()
+}
