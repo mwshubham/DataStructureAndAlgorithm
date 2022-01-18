2913e7e5efc4fe3ddeb3d41795d1b72492583db9
Shubham Agarwal <mwshubham@gmail.com>
2022-01-18 18:25:37 +0530

    75. Sort Colors

--- src/leetcode/problems/category/array/medium/SortColors.kt
+++ src/leetcode/problems/category/array/medium/SortColors.kt
@@ -1,9 +1,15 @@
 package leetcode.problems.category.array.medium
 
 /**
+ * 75. Sort Colors
  * https://leetcode.com/problems/sort-colors/
+ * https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/798/
  */
 class SortColors {
+    /**
+     * Runtime: 176 ms, faster than 57.66% of Kotlin online submissions for Sort Colors.
+     * Memory Usage: 36.5 MB, less than 11.68% of Kotlin online submissions for Sort Colors.
+     */
     fun sortColors(nums: IntArray): Unit {
         var l = 0
         var m = 0
