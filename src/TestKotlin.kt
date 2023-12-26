373376ff0e2bb52865cf848783bd107ff51578b4
Shubham Agarwal <mwshubham@gmail.com>
2023-12-26 15:10:07 +0530

    Update

---
+++ src/TestKotlin.kt
@@ -0,0 +1,14 @@
+import kotlin.math.abs
+
+object TestKotlin {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello World!")
+    }
+
+}
+
+data class Person(
+    val name: String,
+    val age: Int
+)
