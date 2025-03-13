da28962359296e903c8e225a45b7463cef17b20b
Shubham Agarwal <shubham.agarwal@verse.in>
2025-03-14 00:12:56 +0530

    ...

--- src/leetcode/problems/category/array/easy/ContainsDuplicate.kt
+++ src/leetcode/problems/category/array/easy/ContainsDuplicate.kt
@@ -10,8 +10,17 @@ class ContainsDuplicate {
          * Runtime: 248 ms, faster than 72.52% of Kotlin online submissions for Contains Duplicate.
          * Memory Usage: 44.6 MB, less than 44.37% of Kotlin online submissions for Contains Duplicate.
          */
+//        fun containsDuplicate(nums: IntArray): Boolean {
+//          return  nums.distinct().size != nums.size
+//        }
+
         fun containsDuplicate(nums: IntArray): Boolean {
-          return  nums.distinct().size != nums.size
+            val set = HashSet<Int>()
+            nums.forEach {
+                if (set.contains(it)) return true
+                set.add(it)
+            }
+            return false
         }
     }
 }
