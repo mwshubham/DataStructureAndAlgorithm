4c939ff11d62deb0d60d6ea5fc2bb4153d6c6673
Shubham Agarwal <shubham.agarwal@verse.in>
2025-03-29 16:17:08 +0530

    ...

--- src/searching/BinarySearch.kt
+++ src/searching/BinarySearch.kt
@@ -1,29 +1,36 @@
 package searching
 
-val intArray: IntArray = intArrayOf(1, 4, 6, 8, 9, 10)
+import kotlin1.println
 
-fun main() {
-    binarySearch(8)
-    binarySearch(4)
-    binarySearch(5)
-    binarySearch(6)
-    binarySearch(1)
-    binarySearch(10)
-}
+val intArr123: IntArray = intArrayOf(0, 1, 2, 3, 4, 5)
+//val intArray: IntArray = intArrayOf(1, 4, 6, 8, 9, 10)
+val intArray: IntArray = intArrayOf(-1, 0, 3, 5, 9, 12)
 
-fun binarySearch(itemToSearch: Int) {
-    println("Search $itemToSearch")
-    var left = 0
-    var right = intArray.lastIndex
-    while (right >= left) {
-        val mid = left + (right - left) / 2
-        if (intArray[mid] == itemToSearch) {
-            return println("Item [$itemToSearch] is present at position -> $mid")
-        } else if (intArray[mid] < itemToSearch) {
-            left = mid + 1
+fun search(nums: IntArray, target: Int): Int {
+//    if (nums.size == 1 && nums[0] == target) return 0
+//    if (target < nums[0] || target > nums.last()) return -1
+//    if (target == nums[0]) return 0
+//    if (target == nums.last()) return nums.lastIndex
+    var start = 0
+    var end = nums.lastIndex
+    while (start <= end) {
+//        val mid = start + (end + start) / 2
+        val mid = (end + start) / 2
+        val midV = nums[mid]
+        if (target == midV) {
+            return mid
+        } else if (target < midV) {
+            end = mid - 1
         } else {
-            right = mid - 1
+            start = mid + 1
         }
     }
-    return println("Item [$itemToSearch] is not present.")
+    return -1
+}
+
+fun main() {
+    intArray.forEachIndexed { index, i  ->
+        search(intArray, i).println()
+    }
+//    search(intArray, 0).println()
 }
