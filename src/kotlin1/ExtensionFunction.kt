c1241cb6cb3c077ad2d8d542f8bb1731a02b98bb
shubham.agarwal <shubham.agarwal@verse.in>
2025-11-17 17:42:28 +0530

    59. Spiral Matrix II

--- src/kotlin1/ExtensionFunction.kt
+++ src/kotlin1/ExtensionFunction.kt
@@ -2,6 +2,7 @@ package kotlin1
 
 fun <T> T.println() {
     when (this) {
+        is Array<*> -> this.toList().println()
         is IntArray -> println((this as IntArray).asList())
         else -> println(this)
     }
@@ -11,4 +12,8 @@ fun main() {
     IntArray(3).println()
     "string".println()
     123.println()
+    listOf(1, 2, 3).println()
+    Array(4) { 3 }.println()
+    Array(4) { "" }.println()
+    Array(4) { 1.0 }.println()
 }
