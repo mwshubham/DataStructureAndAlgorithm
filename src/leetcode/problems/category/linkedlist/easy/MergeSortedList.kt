d16c7f70e78b5c496e454b517b563ea0ebbfb52f
Shubham Agarwal <mwshubham@gmail.com>
2025-11-27 21:54:24 +0530

    MergeSortedList

--- src/leetcode/problems/category/linkedlist/easy/MergeSortedList.kt
+++ src/leetcode/problems/category/linkedlist/easy/MergeSortedList.kt
@@ -2,57 +2,51 @@ package leetcode.problems.category.linkedlist.easy
 
 import datastructure.ListNode
 
+
+// 1 -> 2 -> 3 -> 51
+// 41 -> 42 -> 43
+
+
+// 2
+// 1
+
 // https://leetcode.com/problems/merge-two-sorted-lists/
 class MergeSortedList {
-    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
-        var list1 = l1
-        var list2 = l2
-        var mergedList: ListNode?
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        if (list1 == null) return list2
+        if (list2 == null) return list1
 
-        if (list1    == null && list2 == null) {
-            return null
+        val head = if (list1.`val` <= list2.`val`) {
+            list1
+        } else {
+            list2
         }
-        if (list1 == null) {
-            return list2
-        }
-        if (list2 == null) {
-            return list1
-        }
-
-        when {
-            list1.`val` <= list2.`val` -> {
-                mergedList = list1
-                list1 = list1.next
+        var node1 = list1
+        var node2 = list2
+        while (node1 != null && node2 != null) {
+            while (node1 != null && node1.`val` <= node2.`val`) {
+                node1 = node1.next
             }
-            else -> {
-                mergedList = list2
-                list2 = list2.next
+            if (node1?.next == null) {
+                node1?.next = node2
+                return head
+            } else {
+                val next = node1.next
+                node1.next = node2
+                node1 = next
             }
-        }
-        val mergedHead = mergedList
-
-        while (list1 != null && list2 != null) {
-            if (list1.`val` <= list2.`val`) {
-                mergedList?.next = list1
-                mergedList = mergedList?.next
-                list1 = list1.next
-            } else if (list1.`val` > list2.`val`) {
-                mergedList?.next = list2
-                mergedList = mergedList?.next
-                list2 = list2.next
+            while (node2?.next != null && node2.next!!.`val` <= node1!!.`val`) {
+                node2 = node2.next
+            }
+            if (node2?.next == null) {
+                node2?.next = node1
+                return head
+            } else {
+                val next = node2.next
+                node2.next = node1
+                node2 = next
             }
         }
-        while (list1 != null) {
-            mergedList?.next = list1
-            mergedList = mergedList?.next
-            list1 = list1.next
-        }
-
-        while (list2 != null) {
-            mergedList?.next = list2
-            mergedList = mergedList?.next
-            list2 = list2.next
-        }
-        return mergedHead
+        return head
     }
 }
