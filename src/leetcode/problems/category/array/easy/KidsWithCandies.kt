028d77cb3b8f276d64c698d8795240b87e7833d0
Shubham Agarwal <mwshubham@gmail.com>
2026-02-23 17:39:58 +0530

    KidsWithCandies

--- src/leetcode/problems/category/array/easy/KidsWithCandies.kt
+++ src/leetcode/problems/category/array/easy/KidsWithCandies.kt
@@ -1,7 +1,9 @@
 package leetcode.problems.category.array.easy
 
-// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
-
+/**
+ * https://leetcode.com/problems/kids-with-the-greatest-number-of-candies
+ * https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+ */
 class KidsWithCandies {
     fun kidsWithCandies(candies: IntArray, extraCandies: Int): BooleanArray {
         val output = BooleanArray(candies.size)
@@ -10,5 +12,13 @@ class KidsWithCandies {
             output[index] = value + extraCandies >= max
         }
         return output
+    }
+
+
+    class Solution {
+        fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
+            val maxCandy = candies.max()
+            return candies.map { it + extraCandies >= maxCandy }
+        }
     }
 }
