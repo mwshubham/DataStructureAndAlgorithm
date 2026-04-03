fdd37b66ccfd718650975af85bdaec894ab2fe1b
Shubham Agarwal <mwshubham@gmail.com>
2026-04-03 15:44:43 +0530

    374. Guess Number Hhigher Or Lower

---
+++ src/leetcode/problems/difficulty/easy/GuessNumberHigherOrLower.kt
@@ -0,0 +1,21 @@
+package leetcode.problems.difficulty.easy
+
+/**
+ * 374. Guess Number Hhigher Or Lower
+ * https://leetcode.com/problems/guess-number-higher-or-lower
+ */
+class GuessNumberHigherOrLower {
+
+//    fun guessNumber(n: Int): Int {
+//        var left = 1
+//        var right = n
+//        while (left <= right) {
+//            val mid = left + (right - left) / 2
+//            val res = guess(mid)
+//            if (res == 0) return mid
+//            else if (res == -1) right = mid - 1
+//            else left = mid + 1
+//        }
+//        return -1
+//    }
+}
